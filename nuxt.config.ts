9cd422b6f96049a694d27eac65c79f67d407de3f
Yahya Baltacı <yahyabltc@gmail.com>
2026-07-27 00:23:11 +0300

    Enable Vercel Web Analytics via the Nuxt module.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@pinia/nuxt'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@pinia/nuxt', '@vercel/analytics/nuxt'],
 
   devtools: {
     enabled: true
